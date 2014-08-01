42bbb2fb8fc99037210ee233f4c2fc65438a588f
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-08-01 15:02:35 -0600

    starting work on random number generator

--- master_mind_integration_test.rb
+++ master_mind_integration_test.rb
@@ -5,8 +5,15 @@ require_relative 'master_mind_introduction'
 
 class IntegrationTest < Minitest::Test
 
+	
+
 	def test_puts_opening_message
-		master_mind = Mastermind.new
-		assert_equal "Welcome to MASTERMIND/n Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+		master_mind = MasterMind.new
+    assert_equal 'Welcome to MASTERMIND Would you like to (p)lay, read the (i)nstructions, or (q)uit?', master_mind.start_up
+	end
+
+	def test_asks_for_a_guess
+		master_mind = MasterMind.new
+		
 	end	
 end
--- master_mind_introduction.rb
+++ master_mind_introduction.rb
@@ -1,18 +1,49 @@
 class MasterMind
 
-	def intro_message
-		puts "Welcome to MASTERMIND/n Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+	def start_up
+		puts 'Welcome to MASTERMIND Would you like to (p)lay, read the (i)nstructions, or (q)uit?'
+		command = ""
+		while command != 'q'
+			command = gets.chomp
+			case command
+			  when 'p'
+			  	puts ""
+				  puts "hi"
+				  puts ""
+				when 'play'
+				  puts "Flaco"  
+			  when 'i'
+				  instructions
+				when 'instructions'
+					instructions   
+			  when 'q'
+				  break
+				when "quit"
+					break	   
+			  else
+			  	puts ""
+				  puts "Not sure what you're trying to do buddy."
+				  intro_message
+			end
+		end
+		puts ""	
+		puts "Thanks for trying out Master Mind."
+		puts ""
 	end
-end
 
 
 
 
+	def instructions
+	puts ""	
+	puts "The computer will create a random four letter code. The codemaker chooses a pattern of four code pegs. Duplicates are allowed, so the player could even choose four code pegs of the same color. The chosen pattern is placed in the four holes covered by the shield, visible to the codemaker but not to the codebreaker."
+	puts ""
+	start_up
+	end
+
+end	
 
 
 
 master_mind = MasterMind.new
-master_mind.intro_message
-
-
-
+master_mind.start_up
