efccfc776d61440e0905af84af5b40a8e3479a45
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-08-07 08:57:23 -0600

    working on game file tests

--- test/gameflow_test.rb
+++ test/gameflow_test.rb
@@ -3,13 +3,14 @@ require_relative "../lib/game"
 
 class GameTest < Minitest::Test
 	attr_reader :game
+	
+
 	def setup
 	end
 
-	def test_outputs_error_message_when_player_guess_too_short
+	def test_takes_an_argument
 	  @game = Game.new(4)
-	  @player_guess = Guess.new("rrrr", 4)	
-	  assert_equal  "Your guess is too long. Try again.", game.create_player_guess 	
+	  assert_equal  4, game.difficulty 
 	end
 
 end	
