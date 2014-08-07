6ce269ab9115695e3abbda8adf5ac2f49f1be658
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-08-07 14:28:30 -0600

    tweaked tests

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -6,35 +6,19 @@ class GuessTest < Minitest::Test
   
   def setup
     @guess = Guess.new("rgby")
-    @upcase_guess = Guess.new("RGBY")
     @invalid_guess = Guess.new("$X679>") 
   end
 
 	def test_returns_an_array
-    assert_equal ['r', 'g', 'b', 'y'], guess.code
+    assert_equal ['R', 'G', 'B', 'Y'], guess.code
   end
 
-  def test_returns_a_downcased_array 
-    assert_equal ['r', 'g', 'b', 'y'], upcase_guess.code  
-  end
 
   def test_returns_correct_number_of_elements
   	assert_equal 4, guess.count
   end
 
-  def test_returns_guess_back_as_string
-  	assert_equal "rgby", guess.guess_as_string
-  end
-
   def test_guess_with_invalid_characters
-    refute false, invalid_guess.valid_characters? 
-  end
-
-  def test_guess_with_invalid_length
-    refute false, invalid_guess.valid_length?
-  end
-
-  def test_valid_method_detects_invalid_guesses
-    refute false, invalid_guess.valid?
+    refute false, invalid_guess.invalid_characters? 
   end
 end	
